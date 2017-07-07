a61da52fdc744505e8d3e4e89f458942be026d80
Daniel <odyright@gmail.com>
2017-07-07 20:44:06 +0000

    Fixed..

---
+++ apps/insc_web/assets/js/Home.js
@@ -0,0 +1,41 @@
+// import S from "skylake"
+// import HomeSticky from "./HomeSticky.js"
+//
+//
+// class Home {
+//   constructor() {
+//     S.BindMaker(this, ["addListeners"])
+//     this.RO = new S.RO({
+//       throttle: {
+//         delay: 100,
+//         atEnd: !0
+//       }
+//     })
+//   }
+//
+//   init() {
+//     // let t = this
+//     this.homeSubmenu = new HomeSticky()
+//     this.homeSubmenu.init()
+//     this.addListeners()
+//   }
+//
+//   addListeners() {
+//     this.listeners("add")
+//   }
+//
+//   listeners(e) {
+//     e === "add" ? this.RO.on() : this.RO.off()
+//   }
+//
+//   destroy() {
+//     this.listeners("remove")
+//     this.homeSubmenu.destroy()
+//   }
+// }
+// //
+// const hom = new Home()
+// hom.init()
+//
+//
+// export default Home
